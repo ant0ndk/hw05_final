2b1efc749d2768fc642d4ea938c52ebf387cb759
Anton <kanton2001@yandex.ru>
2021-10-14 23:35:57 +0300

    fix pep8

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,11 +11,13 @@ urlpatterns = [
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('create/', views.post_create, name='post_create'),
-    path('posts/<int:post_id>/comment/', views.add_comment, name='add_comment'),
-    path('follow/', views.follow_index, name='follow_index'),    
+    path('posts/<int:post_id>/comment/',
+         views.add_comment,
+         name='add_comment'),
+    path('follow/', views.follow_index, name='follow_index'),
     path(
         'profile/<str:username>/follow/',
-        views.profile_follow, 
+        views.profile_follow,
         name='profile_follow'
     ),
     path(
